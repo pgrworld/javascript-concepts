dc9edd3ce8503598d396b12270ca19241a42ffd6
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-08-13 12:47:35 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,220 @@
+
+//.1
+function a(t){
+	console.log(test)
+  console.log(t)
+}
+test = "gowrav"
+a()
+
+//.2
+function a(test){
+	console.log(test)
+}
+test = "gowrav"
+a(test)
+
+//.3
+function a(test){
+	console.log(test)
+}
+test = "gowrav"
+a()
+
+//.4
+function a(test){
+	console.log(test)
+}
+test = "gowrav";
+test = "rahul"
+a(test)
+
+//.5
+function a(test){
+	test = 3;
+	console.log(test);
+}
+
+test = 2;
+a(test);
+
+
+//.6
+function a(test){
+	console.log(test)
+	test = 3;
+	console.log(test);
+}
+test = 2;
+a(test);
+
+//.7
+function a(test){
+	console.log(test2);
+};
+test2 = "1a";
+a()
+
+//.8
+function a(test){
+	console.log(test2);
+	console.log(test)
+};
+test2 = "1a";
+test = "2a";
+a()
+console.log(test)
+
+
+//.9
+function a(test){
+	test2 = "2";
+	console.log(test2);
+	console.log(test);
+}
+test2 = "1a";
+a(1);
+
+
+//.10
+function a(test1){
+	t = "p";
+	test = "1234"
+}
+  t = "1";
+  console.log(t)
+  a();
+  console.log(t);
+  console.log(test);
+
+
+
+//.11
+function a(test){
+	t = "p";
+	test = "1234"
+}
+  t = "1";
+  console.log(t)
+  a();
+  console.log(t);
+  console.log(test);
+
+//.12
+function a(test){
+	t = "p";
+	test = "1234"
+	console.log(test);
+}
+  t = "1";
+  console.log(t)
+  a();
+  console.log(t);
+  
+//.13  
+function a(test){
+  	test = "p";
+  }
+  console.log(a())
+  
+  
+//.14
+function a(test){
+  	return test = "p";
+  }
+  console.log(a())
+  
+//.15
+function a(test){
+  	return test = "p";
+  }
+  a()
+  console.log(test)
+  
+//.16
+function a(test){
+	test = "p";
+	console.log(test);
+}
+test = "t";
+a(test);
+
+
+//.17
+function a(test){
+	test = "p";
+	console.log(test);
+}
+test = "t";
+a();
+console.log(test)
+
+//.18
+function a(test1){
+	test = "p";
+	console.log(test);
+}
+test = "t";
+a();
+console.log(test)
+
+
+//.19
+function a(test){
+	test = "p";
+}
+a("t");
+console.log(test)
+
+//.20
+function a(test1){
+	test = "p";
+}
+a("t");
+console.log(test)
+
+//.21
+function a(test1){
+	test = "p";
+}
+a("t");
+console.log(test)
+test = "s";
+console.log(test)
+
+
+//.22
+function a(test){
+	test = "p";
+}
+   test = "t";
+   a();
+   console.log(test);
+   
+   
+//.23
+function a(t){
+   console.log(test);
+   console.log(t)
+   }
+   test = "t";
+   a(test);
+   test = "p";
+   
+   
+ //.24
+ function a(test){
+	test = "p";
+	console.log(test)
+}
+a()
+console.log(test)
+
+
+//.25
+function a(test1){
+	test = "p";
+	console.log(test)
+}
+a()
+console.log(test)
