7cffa548bfa2c60bd0319078a4a89890631b09e5
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-08-16 13:25:06 +0530

    Update retrun the value form asyn callback func.js

--- retrun the value form asyn callback func.js
+++ retrun the value form asyn callback func.js
@@ -1,12 +1,11 @@
 //IN FILE1.JS
-exports.fun2 = function(fn){
+exports.fun2 = function(returning){
 client.get("key",function(err,res){
-	fn(res);
+	returning(res);
  })
 }
 
 //IN FILE2.JS
-redis.fun2(function(redis){
-	console.log(redis)
+redis.fun2(function(res){
+	console.log(res)
 })
-
